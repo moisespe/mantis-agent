22b616521deb449528baa1eb8e448c2b28557fae
Moises Alvarado <moises0758@gmail.com>
2026-03-23 02:17:30 -0500

    creacion de arrancador de servicio

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Creando arrancador ejecutable
+
+# ===========================================
+# 1 Crear el Servicio 
+
+# 2 Registrarlo en systemd
+
+# 3 Descargar el binario en la ubicacion
