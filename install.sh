d83d4a494ef86a7b3cc33f3be262e60ecb047fff
Moises Alvarado <moises0758@gmail.com>
2026-03-23 02:48:26 -0500

    actualizando arrancador de servicio

--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-# Creando arrancador ejecutable
+SERVICE_NAME=mantis-agente
+BIN_PATH=/usr/local/bin/mantis-agente
+VERSION=0.1.0
 
-# ===========================================
-# 1 Crear el Servicio 
+# descarga binario
+sudo curl https://github.com/moisespe/mantis-agent/releases/download/$VERSION/mantis-agent -o /usr/local/bin/mantis-agent
 
-# 2 Registrarlo en systemd
+# copiar binario
+sudo cp mantis-agente $BIN_PATH
+sudo chmod +x $BIN_PATH
 
-# 3 Descargar el binario en la ubicacion
+# crear servicio
+sudo tee /etc/systemd/system/$SERVICE_NAME.service > /dev/null <<EOF
+[Unit]
+Description=Mantis Agente de Monitoreo
+
+[Service]
+ExecStart=$BIN_PATH
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# recargar y activar
+sudo systemctl daemon-reload
+sudo systemctl enable $SERVICE_NAME
+sudo systemctl start $SERVICE_NAME
+
+echo "Servicio instalado y ejecutándose"
