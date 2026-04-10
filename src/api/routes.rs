9d407b8c35f633e783fb5becc60cdce85ad9c08f
Moises Alvarado <moises0758@gmail.com>
2026-04-10 02:57:36 -0500

    fix | uso de cpu

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -61,7 +61,7 @@ async fn system() -> Json<SystemCore> {
 
 
     Json( SystemCore  {
-        cpu             : cpu,
+        cpu             : cpu * 100.0,
         memory_used     : used_mem / 1024 / 1024,
         memory_total    : total_mem / 1024 / 1024,
         timestamp       : timestamp,
